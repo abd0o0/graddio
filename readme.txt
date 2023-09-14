94c79e31c4b2be8cda68a335bbd75131f11250ec
Abed <38193958+abd0o0@users.noreply.github.com>
2023-09-14 12:28:07 +0200

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,18 @@
+open vs code and in the terminal write " pip install -r requirements.txt "
+with out the double quetations.
+
+you need to install ffmpeg this one is to save the voice you are recording to your files
+to install it you can go to there website or run the command in your power shell 
+to Install Chocolatey type:
+"Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.WebClient]::new().DownloadString('https://chocolatey.org/install.ps1') | i"
+
+Once Chocolatey is installed, run the command 
+choco install ffmpeg
+You can then add the ffmpeg executable to your system's PATH environment variable to be able to run ffmpeg commands from any Command Prompt or PowerShell window.
+
+if everything went fine ;=) then run the project and you will get a local URL open the url and try it 
+
+
+
+
+or you can just run the script befor that  just add the ffmpeg executable to your system's PATH environment variable
---
+++ therapist.py
@@ -0,0 +1,30 @@
+import gradio as gr
+import openai, config, subprocess
+openai.api_key = config.OPENAI_API_KEY
+
+messages = [{"role": "system", "content": 'You are a therapist. Respond to all input in 25 words or less.'}]
+
+def transcribe(audio):
+    global messages
+
+    audio_file = open(audio, "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+
+    messages.append({"role": "user", "content": transcript["text"]})
+
+    response = openai.ChatCompletion.create(model="text-davinci-002", messages=messages)
+
+    system_message = response["choices"][0]["message"]
+    messages.append(system_message)
+
+    subprocess.run(["say", system_message['content']])
+
+    chat_transcript = ""
+    for message in messages:
+        if message['role'] != 'system':
+            chat_transcript += message['role'] + ": " + message['content'] + "\n\n"
+
+    return chat_transcript
+
+ui = gr.Interface(fn=transcribe, inputs=gr.Audio(source="microphone", type="filepath"), outputs="text")
+ui.launch()
